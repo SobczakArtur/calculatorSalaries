0bfb62d4695091f89ae96c536884fb4694021eee
Artur Sobczak <sobczak.artur88@gmail.com>
2023-04-14 23:28:14 +0200

    List and modified methods v1.2

--- src/com/sobczak/artur/EmployedPeople.java
+++ src/com/sobczak/artur/EmployedPeople.java
@@ -2,29 +2,38 @@ package com.sobczak.artur;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeMap;
 
 public class EmployedPeople <E> {
 
-    private List<E> Employee = new ArrayList<>();
+    private List<E> employee = new ArrayList<>();
 
     public void addEmployee(E employe){
-        System.out.println("The person has been added to the list!");
+        System.out.println("Please enter the person's details.");
         System.out.println();
-        Employee.add(employe);
+        employee.add(employe);
+        System.out.println("The person has been added to the list!");
     }
 
     public void removeElement(int itemInTheList){
-        Employee.remove(itemInTheList);
+        System.out.println("Which position are you want remove?");
+        employee.remove(itemInTheList);
         System.out.println("Item " + itemInTheList + "has been removed!");
+    }
+
+    public E updateElements(int index, E element){
+        System.out.println("Which element to be updated: ");
+        System.out.println(employee.get(index));
+        System.out.println("Element " + index + "has been updated!");
+        return employee.set(index, element);
     }
 
     public void printList(){
         System.out.println("Entire list");
-        for (E entireList : Employee) {
+        for (E entireList : employee) {
             System.out.print(entireList + " ");
         }
         System.out.println();
     }
+
 
 }
--- src/com/sobczak/artur/Main.java
+++ src/com/sobczak/artur/Main.java
@@ -8,39 +8,46 @@ public class Main {
 
 
         EmployedPeople<String> employedPeople = new EmployedPeople<>();
-//        Scanner scanner = new Scanner(System.in);
-//        String takeChar = scanner.nextLine();
 
-        char choice = 0;
-        boolean addElements = true;
-        boolean removeElements = true;
+        char choice;
+        boolean addElements = false;
+        boolean removeElements = false;
+        boolean updateElements = false;
+        boolean aboutContinue = true;
 
         System.out.println("What kind of operation on list want to do ?\nAdding elements, delete or update?\nIf you have adding use 'A', remove 'R' or uptade 'U'");
         choice = getChar().charAt(0);
-        if (choice == '+'){
+        if (choice == 'A'){
             addElements = true;
-        } else if (choice == '-'){
+        } else if (choice == 'R'){
             removeElements = true;
+        } else if (choice == 'U'){
+            updateElements = true;
         }
 
-        while (addElements) {
-            System.out.println("Please enter the person's details.");
+        while (addElements && aboutContinue) {
             employedPeople.addEmployee(getDataToList());
-            System.out.println("Are you have add another data to list?\nIf yes enter 'Y', if no enter 'N'");
-            choice = getChar().charAt(0);
-            if (choice == 'Y'){
-
-            } else if (choice == 'N'){
-                addElements = false;
-            }
+//            System.out.println("Are you have add another data to list?\nIf yes enter 'Y', if no enter 'N'");
+//            choice = getChar().charAt(0);
+//            if (choice == 'Y'){
+//
+//            } else if (choice == 'N'){
+//                addElements = false;
+//            }
         }
         employedPeople.printList();
 
-        while (removeElements){
-            employedPeople.removeElement(geListPosition());
+        while (removeElements && aboutContinue){
+            employedPeople.removeElement(getListPosition());
+//            System.out.println("Item " + Main.getListPosition() + "has been removed!");
+            System.out.println("Are you have remove another data from list?\nIf yes enter 'Y', if no enter 'N'");
         }
+        employedPeople.printList();
 
-
+        while (updateElements && aboutContinue){
+            employedPeople.updateElements(getListPosition(), getDataToList());
+        }
+        employedPeople.printList();
 
 
     }
@@ -50,7 +57,24 @@ public class Main {
     public static String getChar() {
         return new Scanner(System.in).nextLine();
     }
-    public static int geListPosition() {
+    public static int getListPosition() {
         return new Scanner(System.in).nextInt();
+    }
+    public static boolean askAboutContinue(char choice, boolean addElements, boolean removeElements, boolean updateElements){
+        boolean aboutContinue;
+        System.out.println("Are you have continue?\nIf yes enter 'Y', if no enter 'N'");
+        choice = getChar().charAt(0);
+        if (choice == 'Y' && addElements == true){
+
+        } else if (choice == 'N' && addElements == true){
+            addElements = false;
+            aboutContinue = addElements;
+        } else if (choice == 'Y' && removeElements == true){
+
+        } else if (choice == 'N' && removeElements == true){
+            removeElements = false;
+            aboutContinue = removeElements;
+
+        return aboutContinue;
     }
 }
